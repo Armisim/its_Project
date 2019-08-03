ce107ecb61748470ed5f0d2a042adf283d35b2ca
Armisim <53098250+Armisim@users.noreply.github.com>
2019-08-03 17:00:56 +0200

    Add files via upload

---
+++ Condizionatore2.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+# Copyright (c)
+# Author: Simone Bollati
+
+# Importazione delle librerie utilizzate all'interno del progetto
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
+import datetime
+import sqlite3 as sqlite
+from Led2 import Led
+from Sensore2 import Sensore
+from Database2 import Database
+
+class Condizionatore:
+    # Costruttore del condizionatore
+    def __init__(self, sensore, led, database, valoreMax):
+        self.sensore = sensore
+        self.led = led
+        self.database = database
+        self.tempMax = valoreMax
+
+    # Ritorna il valore soglia definito nel main
+    def getValoreMax(self):
+        return self.tempMax
+
+    # Ritorna la data e l'orario
+    def getData(self):
+        return datetime.datetime.utcnow()
